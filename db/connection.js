a6b40ce99a8b668f45787571c7b243b7dee18433
shhahidd <patelshahid260@gmail.com>
2026-07-08 15:29:52 +0530

    fix: lazy load sqlite3

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,3 @@
-const sqlite3 = require('sqlite3').verbose();
 const { Pool } = require('pg');
 const path = require('path');
 
@@ -68,6 +67,7 @@ if (process.env.DATABASE_URL) {
     }
   };
 } else {
+  const sqlite3 = require('sqlite3').verbose();
   const dbPath = path.resolve(__dirname, '../portfolio.db');
   db = new sqlite3.Database(dbPath, (err) => {
     if (err) {
